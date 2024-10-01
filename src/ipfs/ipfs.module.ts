fcbc13a4a66ec38033bd7e9f1b41db398d95509d
farrellh1 <farrell.hauzan@gmail.com>
2024-10-01 18:50:33 +0700

    fix(ipfs): mongoose module for event

--- src/ipfs/ipfs.module.ts
+++ src/ipfs/ipfs.module.ts
@@ -3,9 +3,14 @@ import { IpfsService } from './ipfs.service';
 import { IpfsController } from './ipfs.controller';
 import { EventsModule } from 'src/events/events.module';
 import { EventsService } from 'src/events/events.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Event, EventSchema } from 'src/events/schemas/event.schema';
 
 @Module({
-  imports: [EventsModule],
+  imports: [
+    EventsModule,
+    MongooseModule.forFeature([{ name: Event.name, schema: EventSchema }]),
+  ],
   controllers: [IpfsController],
   providers: [IpfsService, EventsService],
 })
